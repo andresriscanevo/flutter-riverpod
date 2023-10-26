e5685ef9a921954df50179fb3d992b5f63fd60b6
andresriscanevo <andresriscanevo10@gmail.com>
2023-10-26 13:08:50 -0500

    manejo de future provider y stream provider

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -1,3 +1,5 @@
 export 'simple_name_provider.dart';
 export 'states_providers.dart';
 export 'todo_providers.dart';
+export 'futures_providers.dart';
+export 'stream_provider.dart';
--- lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
+++ lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/providers.dart';
 
 class StreamProviderScreen extends StatelessWidget {
   const StreamProviderScreen({super.key});
@@ -10,6 +11,30 @@ class StreamProviderScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Stream Provider'),
       ),
+      body: StreamView(),
+    );
+  }
+}
+
+class StreamView extends ConsumerWidget {
+  const StreamView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userInchat = ref.watch(usersInChatProvider);
+    if (!userInchat.hasValue) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return ListView.builder(
+      itemCount: userInchat.value!.length,
+      itemBuilder: (BuildContext context, int index) {
+        final name = userInchat.value![index];
+        return ListTile(
+          title: Text(name),
+        );
+      },
     );
   }
 }
