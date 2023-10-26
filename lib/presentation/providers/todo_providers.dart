3ccf1dacf99bedc2a70ecbc17ae6f8ce08aac0ba
andresriscanevo <andresriscanevo10@gmail.com>
2023-10-26 11:34:18 -0500

    mezcla de providers

---
+++ lib/presentation/providers/todo_providers.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/config/config.dart';
+import 'package:riverpod_app/domain/entities/todo.dart';
+import 'package:uuid/uuid.dart';
+
+enum TodoFilter { all, completed, pending }
+
+const uuid = Uuid();
+
+final todoFilterProvider = StateProvider<TodoFilter>((ref) {
+  return TodoFilter.all;
+});
+final todosProvider = StateProvider<List<Todo>>((ref) {
+  return [
+    Todo(
+      id: uuid.v4(),
+      description: RandomGenerator.getRandomName(),
+      completedAt: null,
+    ),
+    Todo(
+        id: uuid.v4(),
+        description: RandomGenerator.getRandomName(),
+        completedAt: DateTime.now()),
+    Todo(
+      id: uuid.v4(),
+      description: RandomGenerator.getRandomName(),
+      completedAt: null,
+    ),
+    Todo(
+      id: uuid.v4(),
+      description: RandomGenerator.getRandomName(),
+      completedAt: DateTime.now(),
+    ),
+    Todo(
+      id: uuid.v4(),
+      description: RandomGenerator.getRandomName(),
+      completedAt: null,
+    ),
+  ];
+});
+final filterTodosProvider = Provider<List<Todo>>((ref) {
+  final selectedFilter = ref.watch(todoFilterProvider);
+  final todos = ref.watch(todosProvider);
+  switch (selectedFilter) {
+    case TodoFilter.all:
+      return todos;
+
+    case TodoFilter.completed:
+      return todos.where((element) => element.done).toList();
+
+    case TodoFilter.pending:
+      return todos.where((element) => !element.done).toList();
+  }
+});
