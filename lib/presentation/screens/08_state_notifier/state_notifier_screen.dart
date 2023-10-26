f279e47e27fda3a3638d1119914c8deb9ba34af3
andresriscanevo <andresriscanevo10@gmail.com>
2023-10-26 13:47:36 -0500

    creacion y manipulacion del state notifier provider

--- lib/presentation/screens/08_state_notifier/state_notifier_screen.dart
+++ lib/presentation/screens/08_state_notifier/state_notifier_screen.dart
@@ -1,15 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/providers.dart';
+import 'package:riverpod_app/presentation/providers/state_notifier_providers.dart';
 
-
-class StateNotifierScreen extends StatelessWidget {
+class StateNotifierScreen extends ConsumerWidget {
   const StateNotifierScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('State Notifier Provider'),
       ),
+      body: TodosView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add_a_photo),
+        onPressed: () {
+          ref.read(todosNotifierProvider.notifier).addTodo();
+        },
+      ),
+    );
+  }
+}
+
+class TodosView extends ConsumerWidget {
+  const TodosView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentFilter = ref.watch(todoFilterProvider);
+    final todos = ref.watch(filteredGuestProvider);
+    return Column(
+      children: [
+        const ListTile(
+          title: Text('Listado de invitados'),
+          subtitle: Text('Estas son las personas a invitar a la fiesta'),
+        ),
+        SegmentedButton(
+          segments: const [
+            ButtonSegment(value: TodoFilter.all, icon: Text('Todos')),
+            ButtonSegment(value: TodoFilter.completed, icon: Text('Invitados')),
+            ButtonSegment(
+                value: TodoFilter.pending, icon: Text('No invitados')),
+          ],
+          selected: <TodoFilter>{currentFilter},
+          onSelectionChanged: (value) {
+            ref.read(todoFilterProvider.notifier).state = value.first;
+          },
+        ),
+        const SizedBox(height: 5),
+        Expanded(
+          child: ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (context, index) {
+              final todo = todos[index];
+              return SwitchListTile(
+                  title: Text(todo.description),
+                  value: todo.done,
+                  onChanged: (value) {
+                    ref
+                        .read(todosNotifierProvider.notifier)
+                        .toggleTodo(todo.id);
+                  });
+            },
+          ),
+        )
+      ],
     );
   }
 }
